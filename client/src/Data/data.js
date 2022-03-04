d4c4d0cc111a8c511a689a2c6177c85d1d8527e1
coding-to-music <connors.tom@gmail.com>
2022-03-04 06:49:45 +0000

    build

--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -1,7 +1,5 @@
 export const projectData = {
-  repositoryUrl:
-    "https://github.com/coding-to-music/employee-management-dashboard",
+  repositoryUrl: "https://github.com/coding-to-music/employee-management-dashboard",
   graphqlServerLocal: "http://localhost:4000/graphql",
-  graphqlServerLive:
-    "https://employee-management-dashboard.herokuapp.com/graphql",
+  graphqlServerLive: "https://employee-management-dashboard.herokuapp.com/graphql",
 };
